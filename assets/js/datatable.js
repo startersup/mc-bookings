07fc0e07b313d3520676b275703f5520bad5f661
saicharan <saicharan14996@gmail.com>
2020-01-26 16:43:06 +0530

    xcx
    added sync for table

--- assets/js/datatable.js
+++ assets/js/datatable.js
@@ -1,3 +1,85 @@
+function otherFunction() {
+  document.getElementById("mc-dropdown").classList.toggle("show");
+}
 
+window.onclick = function(event) {
+  if (!event.target.matches(".mc-others-icon")) {
+    var dropdowns = document.getElementsByClassName("dropdown-set");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
 
+$(document).ready(function() {
+  $("#myInput").on("keyup", function() {
+    var value = $(this)
+      .val()
+      .toLowerCase();
+    $("#mc-datatables tr").filter(function() {
+      $(this).toggle(
+        $(this)
+          .text()
+          .toLowerCase()
+          .indexOf(value) > -1
+      );
+    });
+  });
+});
 
+$(document).ready(function() {
+  $("#mc-datatables").DataTable({
+    dom: "Bfrtip",
+    colReorder: true,
+    buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+    lengthChange: false,
+    searching: false,
+    fixedHeader: true,
+    pageLength: 25,
+    scrollY: "700px",
+    scrollX: true,
+    ordering: false,
+    scrollCollapse: true,
+    paging: true,
+    fixedColumns: {
+      leftColumns: 1,
+      rightColumns: 1
+    }
+  });
+});
+
+function alerttest()
+{
+
+    alert("jhcsd");
+
+}
+var count =0;
+function addrow()
+{
+    count++;
+    var t = $('#mc-datatables').DataTable();
+    t.row.add( [
+        'BRT'+count,
+        '.Gatwick',
+        'Heathrow Airport Terminal Number 2',
+        '.West sussex crawley road, horsham Rh11 1nf',
+        '.12/07/2019 12:45',
+        'Saloon x 1',
+        '234 GBP',
+        '<div class="green-status">Booked</div>',
+        'Edit'
+    ] ).draw( false );
+}
+
+function addcustomrow()
+{
+    for(var i=0;i<80;i++)
+    {
+        addrow();
+    }
+}
---
+++ assets/js/server.js
@@ -0,0 +1,61 @@
+
+var text = '{'+
+' "booked" : { "class":"green-status" , "showstatus":"Booked" },'+
+' "booked-confirmed" : { "class":"green-status" , "showstatus":"Confirmed" },'+
+' "comitted" : { "class":"green-status" , "showstatus":"Allocated" },'+
+' "cancelled" : { "class":"green-status" , "showstatus":"Cancelled" },'+
+' "completed" : { "class":"green-status" , "showstatus":"Completed" }'+
+          '}';
+var obj_status = JSON.parse(text);
+function get_url_response()
+{
+               // document.getElementById("spinnermodal").style.display = "block";
+               var url="https://minicabee.co.uk/myadmin/myapi/func_future.php";
+                $.ajax({
+                    
+                type: 'POST',
+                url:url,
+                data: '',
+                async:false,
+                success: function(data) 
+                { 
+
+                    setRow(data);
+                    // document.getElementById("spinnermodal").style.display = "none";
+                     //window[functionName](data);
+                },
+                error: function(xhr) { 
+                    alert("fail")
+                   // document.getElementById("spinnermodal").style.display = "none";
+                  //  Server_Response_Fail(xhr.responseText); 
+                }
+                });
+
+}
+
+function setRow(obj)
+{
+    var myTable = $('#mc-datatables').DataTable();
+    var loop = obj.length();
+    var editRec='<div class="mc-edit"></div>';
+    var statusRec = GetRecordStatus(obj[i]["status"]);
+    for(var i=0;i<loop;i++)
+    {
+        t.row.add( [
+            obj[i]["refid"],
+            obj[i]["booked_site"],
+            obj[i]["src"],
+            obj[i]["des"],
+            obj[i]["dt"]+' '+obj[i]["time"],
+            obj[i]["type"],
+            obj[i]["fare"],
+            statusRec,           
+            editRec
+        ] ).draw( false );
+    }
+}
+function GetRecordStatus(status)
+{
+
+    return '<div class="'+obj_status[status]["class"]+'">'+obj_status[status]["showstatus"]+'</div>';
+}
