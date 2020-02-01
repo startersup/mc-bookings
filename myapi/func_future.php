caa0361c1aa303819b5207f712f3ac3f76f4d6e3
saicharan <saicharan14996@gmail.com>
2020-02-01 13:23:12 +0530

    ok
    ok

--- myapi/func_future.php
+++ myapi/func_future.php
@@ -15,7 +15,7 @@
     $sql="SELECT refid,src,des,dt,time,type,fare,dfare,status,booked_site from register WHERE dt> '".$check."' and status != 'cancelled' ";
   $result=  mysqli_query($conn,$sql);
   
-  
+  echo("<br>".$sql);
   while( $row= mysqli_fetch_array($result,MYSQLI_ASSOC))
   {
      
