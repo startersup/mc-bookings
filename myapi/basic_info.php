b137b003d4f5ba1fa7ebab8bdb48b8c02837e194
saicharan <saicharan14996@gmail.com>
2020-02-01 14:57:28 +0530

    basic info
    ok

---
+++ myapi/basic_info.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+ $rootfolder= $_SERVER['DOCUMENT_ROOT']; 
+ 
+  include($rootfolder."/connection/connect.php"); 
+
+
+  
+    $book_id=$_POST["book_id"];
+
+   
+  
+    
+    $sql="SELECT booked_site,status,src,des,miles,jtime,dt,time,type from register WHERE refid> '".$book_id."' ";
+    $result=  mysqli_query($conn,$sql);
+
+ $row= mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+     
+     $temp[]=$row;
+
+  echo  json_encode($temp);
+
+
+ 
