0ed78ca06637c31e18a7d0714fadecb3688c1d02
saicharan <saicharan14996@gmail.com>
2020-02-02 22:32:30 +0530

    ok
    ok

--- myapi/custom.php
+++ myapi/custom.php
@@ -20,7 +20,7 @@ session_start();
     $sql = $sql + $status;
   }
     
-    $sql="SELECT refid,src,des,dt,time,type,fare,dfare,status,booked_site from register WHERE dt= '".$check."' and status != 'cancelled' ";
+  //  $sql="SELECT refid,src,des,dt,time,type,fare,dfare,status,booked_site from register WHERE dt= '".$check."' and status != 'cancelled' ";
     $result=  mysqli_query($conn,$sql);
     
     
