4d5fad47d216bc0077cc1e7cb9d5b80bce6982cb
saicharan <saicharan14996@gmail.com>
2020-02-15 08:23:41 +0530

    new
    new

--- myapi/book.php
+++ myapi/book.php
@@ -5,7 +5,33 @@ session_start();
  $rootfolder= $_SERVER['DOCUMENT_ROOT']; 
  
   include($rootfolder."/connection/connect.php"); 
-
+  $name=$_POST['name'];
+  $mail=$_POST['mail'];
+  $mobile1=$_POST['mobile1'];
+  $mobile2=$_POST['mobile2'];
+  $location=$_POST['location'];
+  $info=$_POST['info'];
+  $pay=$_POST['pay'];
+  $origin_input=$_POST['origin-input'];
+  $destination_input=$_POST['destination-input'];
+  $address1=$_POST['address1'];
+  $address2=$_POST['address2'];
+  $date=$_POST['date'];
+  $time=$_POST['time'];
+  $np=$_POST['np'];
+  $nl=$_POST['nl'];
+  $cabType=$_POST['cabType'];
+  $np2=$_POST['np2'];
+  $time=$_POST['time'];
+  $fare=$_POST['fare'];
+  $status=$status;
+  $via=$_POST['via'];
+  $dfare=$dfare;
+  $meet=$_POST['meet'];
+  $child=$_POST['child'];
+  $distance=$_POST['distance'];
+  $booked_site=$_POST['booked_site'];
+  $drvid=$_POST['drvid'];
  if($_POST["drvid"]=="")
  {
 	 $dfare=$_POST["fare"]/100;    
@@ -46,8 +72,8 @@ if($check==0)
 
 $ref=$ran;
     
-$sql="INSERT INTO `register` (`refid`, `name`, `mail`, `num1`, `num2`, `location`, `info`, `pay`, `src`, `des`,`address1`,`address2`, `dt`, `time`, `passenger`, `luggage`, `type`, `infants`, `jtime`, `fare`, `status`, `via`, `dfare`, `mg`, `ceat`, `miles`,`booked_site`,`drvid`) VALUES ($ref, $_POST['name'], $_POST['mail'], $_POST['mobile1'], $_POST['mobile2'], $_POST['location'], $_POST['info'], $_POST['pay'], $_POST['origin-input'], $_POST['destination-input'],$_POST['address1'],$_POST['address2'], $_POST['date'], $_POST['time'], $_POST['np'], $_POST['nl'], $_POST['cabType'], $_POST['np2'], $_POST['time'], $_POST['fare'], $status, $_POST['via'], $dfare, $_POST['meet'], $_POST['child'], $_POST['distance'],$_POST['booked_site'],$_POST['drvid'])";
-
+$sql="INSERT INTO `register` (`refid`, `name`, `mail`, `num1`, `num2`, `location`, `info`, `pay`, `src`, `des`,`address1`,`address2`, `dt`, `time`, `passenger`, `luggage`, `type`, `infants`, `jtime`, `fare`, `status`, `via`, `dfare`, `mg`, `ceat`, `miles`,`booked_site`,`drvid`) 
+VALUES ($ref, '$name', '$mail', '$mobile1', '$mobile2', '$location', '$info', '$pay', '$origin_input', '$destination_input','$address1','$address2', '$date', '$time', '$np', '$nl', '$cabType', '$np2', '$time', '$fare', $status, '$via', $dfare, '$meet', '$child', '$distance','$booked_site','$drvid')";
  $user= mysqli_query(conn,$sql) ;
  
  if($user)
@@ -59,4 +85,6 @@ else{
 		 $temp["status"]="fail";
 	 $temp["message"]="Try Again";
 }
+
+echo json_encode($temp);
  ?>
