5129c1f0bbc620ee37fadab095f77d343799423b
saicharan <saicharan14996@gmail.com>
2020-03-26 23:09:52 +0530

    driver

--- myapi/DriverList.php
+++ myapi/DriverList.php
@@ -1,9 +1,3 @@
-<?php
-$time = microtime();
-$time = explode(' ', $time);
-$time = $time[1] + $time[0];
-$start = $time;
-?>
 
 <?php
 
@@ -35,11 +29,3 @@ if($temp==null)
   ?>
 
  
-<?php
-$time = microtime();
-$time = explode(' ', $time);
-$time = $time[1] + $time[0];
-$finish = $time;
-$total_time = round(($finish - $start), 4);
-echo 'Page generated in '.$total_time.' seconds.';
-?>
