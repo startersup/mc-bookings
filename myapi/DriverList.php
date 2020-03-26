2cf9c6a0dd54174b04591a392ea34a7855720133
saicharan <saicharan14996@gmail.com>
2020-03-26 22:20:39 +0530

    time check

--- myapi/DriverList.php
+++ myapi/DriverList.php
@@ -1,4 +1,11 @@
 <?php
+$time = microtime();
+$time = explode(' ', $time);
+$time = $time[1] + $time[0];
+$start = $time;
+?>
+
+<?php
 
 session_start();
 
@@ -28,3 +35,11 @@ if($temp==null)
   ?>
 
  
+<?php
+$time = microtime();
+$time = explode(' ', $time);
+$time = $time[1] + $time[0];
+$finish = $time;
+$total_time = round(($finish - $start), 4);
+echo 'Page generated in '.$total_time.' seconds.';
+?>
