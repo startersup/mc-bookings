4ece25cd498ba7673a6cf373d8084d795601be61
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-06-24 23:21:01 +0530

    conn issue

--- connection/connect.php
+++ connection/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn=mysqli_connect('sql124.main-hosting.eu','u678426119_mini','myproject','u678426119_mini');
+$conn=mysqli_connect('sql131.main-hosting.eu','u678426119_mini','myproject','u678426119_mini');
 
 
 ?>
