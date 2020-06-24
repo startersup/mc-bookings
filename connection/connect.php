a9d7b4369619d30bdb83bdcea0d1dda3b6b43d18
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-06-24 23:38:05 +0530

    connection

--- connection/connect.php
+++ connection/connect.php
@@ -1,13 +1,5 @@
 <?php
 
 $conn=mysqli_connect('sql131.main-hosting.eu','u678426119_mini','myproject','u678426119_mini');
-$coon_val="";
-if($conn)
-{
-    $conn_val="true";
-}else{
-    $conn_val="false";
-}
-echo "<script>console.log('.$conn_val.'); </script>";
 
 ?>
