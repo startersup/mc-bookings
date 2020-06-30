b4cc7a71ed79f6303866f20c50d3d57030131d66
Saicharan <saicharan14996@gmail.com>
2020-06-30 19:51:12 +0530

    Update index.php
    booking traffic card added

--- index.php
+++ index.php
@@ -240,7 +240,10 @@
 <div class="quick-map-lookups">
   <div class="row">
   <div class="col-md-12">
+    <div class="mc-metric-card">
+      <h3>Booking Traffic</h4>
   <div id="map" class="mc-cluster-map"></div>
+</div>
 </div></div></div>
 </div>
 
@@ -324,8 +327,8 @@
 function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
-    center: {lat: -28.024, lng: 140.887}
+    zoom: 8,
+    center: {lat: 55.3781, lng: 3.4360}
   });
 
   // Create an array of alphabetical characters used to label the markers.
@@ -347,29 +350,17 @@ function initMap() {
       {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
 var locations = [
-  {lat: -31.563910, lng: 147.154312},
-  {lat: -33.718234, lng: 150.363181},
-  {lat: -33.727111, lng: 150.371124},
-  {lat: -33.848588, lng: 151.209834},
-  {lat: -33.851702, lng: 151.216968},
-  {lat: -34.671264, lng: 150.863657},
-  {lat: -35.304724, lng: 148.662905},
-  {lat: -36.817685, lng: 175.699196},
-  {lat: -36.828611, lng: 175.790222},
-  {lat: -37.750000, lng: 145.116667},
-  {lat: -37.759859, lng: 145.128708},
-  {lat: -37.765015, lng: 145.133858},
-  {lat: -37.770104, lng: 145.143299},
-  {lat: -37.773700, lng: 145.145187},
-  {lat: -37.774785, lng: 145.137978},
-  {lat: -37.819616, lng: 144.968119},
-  {lat: -38.330766, lng: 144.695692},
-  {lat: -39.927193, lng: 175.053218},
-  {lat: -41.330162, lng: 174.865694},
-  {lat: -42.734358, lng: 147.439506},
-  {lat: -42.734358, lng: 147.501315},
-  {lat: -42.735258, lng: 147.438000},
-  {lat: -43.999792, lng: 170.463352}
+  {lat: 51.1537, lng: 0.1821},
+  {lat: 51.5074, lng: 0.1278},
+  {lat: 51.1537, lng: 0.1821},
+  {lat: 51.1537, lng: 0.1821},
+  {lat: 51.1537, lng: 0.1821},
+  {lat: 51.1537, lng: 0.1821},
+  {lat: 51.5074, lng: 0.1278},
+  {lat: 51.5074, lng: 0.1278},
+  {lat: 2.4862, lng: 1.8904},
+  {lat: 2.4862, lng: 1.8904},
+  {lat: 2.4862, lng: 1.8904}
 ]
 </script>
 <!-- Replace following script src -->
