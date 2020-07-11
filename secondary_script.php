6c2df072be69cd4db15fde9d246d542726edd474
Saicharan <saicharan14996@gmail.com>
2020-07-11 14:44:22 +0530

    update js
    update js

--- secondary_script.php
+++ secondary_script.php
@@ -10,6 +10,80 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>  
 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.js"></script>
+<script type="text/javascript" src="./assets/js/custom-calender.js"></script>
+<script type="text/javascript">
+
+    $(function () {
+        $('#wrapper .version strong').text('v' + $.fn.pignoseCalendar.version);
+
+        function onSelectHandler(date, context) {
+
+            var $element = context.element;
+            var $calendar = context.calendar;
+            var $box = $element.siblings('.box').show();
+            var text = '';
+
+            if (date[0] !== null) {
+                text += date[0].format('YYYY-MM-DD');
+            }
+
+            if (date[0] !== null && date[1] !== null) {
+                text += ' ~ ';
+            }
+            else if (date[0] === null && date[1] == null) {
+                text += 'nothing';
+            }
+
+            if (date[1] !== null) {
+                text += date[1].format('YYYY-MM-DD');
+            }
+
+            $box.text(text);
+        }
+
+        function onApplyHandler(date, context) {
+   
+            var $element = context.element;
+            var $calendar = context.calendar;
+            var $box = $element.siblings('.box').show();
+            var text = 'You applied date ';
+
+            if (date[0] !== null) {
+                text += date[0].format('YYYY-MM-DD');
+            }
+
+            if (date[0] !== null && date[1] !== null) {
+                text += ' ~ ';
+            }
+            else if (date[0] === null && date[1] == null) {
+                text += 'nothing';
+            }
+
+            if (date[1] !== null) {
+                text += date[1].format('YYYY-MM-DD');
+            }
+
+            $box.text(text);
+        }
+
+
+
+        // Blue theme type Calendar
+        $('.calendar-blue').pignoseCalendar({
+            theme: 'blue', // light, dark, blue
+            select: onSelectHandler
+        });
+
+
+        $('.menu .item').tab();
+    });
+    //]]>
+</script>
+
+
+
 <!-- <script src="./assets/js/common.js"></script>
 <script src="./assets/js/server.js"></script>
 <script src="../assets/js/router.js"></script>
