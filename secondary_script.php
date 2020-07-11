1ce065b6440a2f4afd075ddcec981535b2e90dbb
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-07-11 13:41:04 +0530

    versionate

--- secondary_script.php
+++ secondary_script.php
@@ -10,9 +10,20 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>  
 
-<script src="./assets/js/common.js"></script>
+<!-- <script src="./assets/js/common.js"></script>
 <script src="./assets/js/server.js"></script>
 <script src="../assets/js/router.js"></script>
   <script src="./assets/js/sparkline-chart.js"></script>
   <script src="./assets/js/dashboard.js"></script>
-  <script src="../assets/js/datatable.js"></script>
+  <script src="../assets/js/datatable.js"></script> -->
+
+  <script src="./assets/js/sparkline-chart.js"></script>   
+  <script>
+  var version=  Math.floor(Math.random() * 100);
+  document.write('<script src="./assets/js/common.js?dev=' +version + '"\><\/script>');
+  document.write('<script src="./assets/js/server.js?dev=' +version + '"\><\/script>');
+  document.write('<script src="./assets/js/router.js?dev=' +version + '"\><\/script>');
+  document.write('<script src="./assets/js/dashboard.js?dev=' +version + '"\><\/script>');
+  document.write('<script src="./assets/js/datatable.js?dev=' +version + '"\><\/script>'); 
+ 
+  </script>
