83292c50a32d7ceccf1bc2bbed27a4f61be8df37
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-09-12 17:53:37 +0530

    Message 2

---
+++ myapi/message2.php
@@ -0,0 +1,35 @@
+<?php
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"https://api.twilio.com/2010-04-01/Accounts/AC093fdc747b395f0d5d0b7dce029ea20e/Messages.json");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "From=+447723012660&To=+7818065914&Body=Test");
+
+$headers = array(
+    'Content-Type:application/x-www-form-urlencoded',
+    'Authorization: Basic '. base64_encode("AC093fdc747b395f0d5d0b7dce029ea20e:6d4e9f5c8fc58e9eac02b65d4bdb06c9")
+);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+
+// receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+// further processing ....
+if ($server_output == "OK") { 
+    
+    echo("Success <br>");
+    echo json_encode($server_output);
+ } else { 
+     
+    echo("Fail <br>");
+    echo json_encode($server_output);
+  }
+
+?>
