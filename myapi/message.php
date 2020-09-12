860f3dcedefea8eec507327266094b4ef0e6a91e
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-09-12 17:28:25 +0530

    message api

---
+++ myapi/message.php
@@ -0,0 +1,23 @@
+<?php
+$from='+447723012660';
+$to = '+7818065914';
+$msg='Test';
+$url ="https://api.twilio.com/2010-04-01/Accounts/AC093fdc747b395f0d5d0b7dce029ea20e/Messages.json";
+$data = array('From' => $from, 'To' => $to,'Body' => $msg);
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded , Authorization: Basic ". base64_encode("AC093fdc747b395f0d5d0b7dce029ea20e:6d4e9f5c8fc58e9eac02b65d4bdb06c9"),
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+
+var_dump($result);
+echo json_encode($result)
+?>
+
