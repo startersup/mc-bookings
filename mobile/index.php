ea4343710a2838e64dca62412dd735f639e25fe7
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-10-01 12:48:08 +0530

    js file include

--- mobile/index.php
+++ mobile/index.php
@@ -14,6 +14,8 @@
   <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+  <script src="../assets/js/mobileBooking.js"></script>
+ 
 </head>
 <body >
 <div style="display:none;" id="map"></div>
