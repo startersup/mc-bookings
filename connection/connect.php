274d113ee7acb21ed76da6ecbd32f5816064b8ff
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-10-01 15:08:36 +0530

    mysql password

--- connection/connect.php
+++ connection/connect.php
@@ -1,8 +1,5 @@
 <?php
 
-$conn=mysqli_connect('sql131.main-hosting.eu','u678426119_mini','myproject','u678426119_mini');
-if(!$conn)
-{
-    echo("Conn Failed");
-}
+$conn=mysqli_connect('sql131.main-hosting.eu','u678426119_mini','Minicabee@123','u678426119_mini');
+
 ?>
