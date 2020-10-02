8b0372fff504df6b15578dd62b7621827ed3b603
Saicharan <saicharan14996@gmail.com>
2020-10-02 12:09:48 +0530

    page udpatesssss
    page udpatesssss

--- mobile/index.php
+++ mobile/index.php
@@ -44,7 +44,7 @@
   </div>
  </nav> 
 
- <div class=" mc-alert-box alert-dismissible" id="myAlert" style="display: none;">
+ <div class=" mc-alert-box" id="myAlert" style="display: none;">
       <div class="mc-alert-status color-green" id="myAlert_color" ><p id="myAlert_status" ><svg height="40" viewBox="0 0 515.556 515.556" width="40" xmlns="http://www.w3.org/2000/svg"><path d="M0 274.226l176.549 176.886L515.556 112.44l-48.67-47.997-290.337 290L47.996 225.891z"/></svg></p></div>
       <div class="mc-alert-desc " >     <a href="#" onclick="showStatusMessageClose();" class="close" data-dismiss="alert" aria-label="close">&times;</a><p id="myAlert_msg"> hII there</p></div>
     </div>
