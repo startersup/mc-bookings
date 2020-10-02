a489e20ecae50323af7d6c479077f716fbff8d1d
Saicharan <saicharan14996@gmail.com>
2020-10-02 12:08:25 +0530

    page update
    page update

--- mobile/index.php
+++ mobile/index.php
@@ -26,7 +26,7 @@
   background-color: rgb(255, 255, 255);
   min-width: 1500px;
   top: 0px;
-  left: 65px;
+  left: 0px;
   border: none;">
     <div class="modal11-content">
       <div class="loader" style="max-width:100px;position:absolute;top:300px;left:650px;">
