78ecf94c010737513d01cb4b7dcd0a552430753b
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-10-08 15:03:43 +0530

    conn

--- connection/connect.php
+++ connection/connect.php
@@ -3,7 +3,11 @@
 $conn=mysqli_connect('sql131.main-hosting.eu','u678426119_mini','Minicabee@123','u678426119_mini');
 if(!$conn)
 {
-    echo("Conn Failed... <br>".$conn);
+    echo("Conn Failed... <br> err".$conn);
 
 }
+if ($conn -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn -> connect_error;
+   
+  }
 ?>
