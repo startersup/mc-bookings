36d86b8c55591a1103fd75630facd0b52563e73b
Saicharan <saicharan14996@gmail.com>
2020-10-11 18:36:15 +0530

    Update index.php
    css update

--- settings/promo/index.php
+++ settings/promo/index.php
@@ -27,10 +27,12 @@ if($action =="")
           <div class="row">
               <div class="col-md-5">
           <h3>Promo Settings</h3></div>
-          <div class="col-md-7">
+          <div class="col-md-7"><br><br>
+          <div class="mc-flex">
           <select class="select form-control siteDropDown" required="" id="siteId" >
-        
         </select>  
+        <button type="submit" class="btn btn-success">Add Promo +</button>
+</div>
             </div>
       </div>
         </div>
