3cea15bedb15fdb337d85eaf4ca1a42c1e44880d
Deepak12051996 <36324331+Deepak12051996@users.noreply.github.com>
2020-10-11 11:22:49 +0530

    handling website with user

--- login/getUserInfo.php
+++ login/getUserInfo.php
@@ -2,12 +2,12 @@
 
 session_start();
 
-$prev_url=$_SERVER['HTTP_REFERER'];
-$host=$_SERVER['HTTP_HOST'];
-if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') 
-    $link = "https"; 
+$prev_url = $_SERVER['HTTP_REFERER'];
+$host = $_SERVER['HTTP_HOST'];
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    $link = "https";
 else
-    $link = "http"; 
+    $link = "http";
 date_default_timezone_set('Europe/London');
 if (strpos($prev_url, $host)) {
     $rootfolder = $_SERVER['DOCUMENT_ROOT'];
@@ -18,7 +18,7 @@ if (strpos($prev_url, $host)) {
     } else {
         $user = $_POST["user"];
         $password = $_POST["password"];
-        $query = "SELECT `name`,`mail`,`password`,`username`,`rights` from user WHERE ( username = '" . $user . "'  or mail = '" . $user . "' or mobile1 = '" . $user . "' ) and status != 'dismissed' ";
+        $query = "SELECT `name`,`mail`,`password`,`username`,`rights`,`booking_Rights` from user WHERE ( username = '" . $user . "'  or mail = '" . $user . "' or mobile1 = '" . $user . "' ) and status != 'dismissed' ";
         $result =  mysqli_query($conn, $query);
         $count = mysqli_num_rows($result);
         if ($count > 0) {
@@ -29,33 +29,45 @@ if (strpos($prev_url, $host)) {
                 $_SESSION["mail"] = $row["mail"];
                 $output = $row["rights"];
 
+                $_SESSION["registerClause"] = "";
+
+                if (!($row["booking_Rights"] == "" || strtoupper($row["booking_Rights"]) == "ALL")) {
+                    $tags = explode(',', $row["booking_Rights"]);
+                    $_SESSION["registerClause"] .= " AND (";
+                    $loopcount = count($tags);
+                    for ($i = 0; $i < $loopcount; $i++) {
+                        $_SESSION["registerClause"].= " ( `refid` like '".$tags[$i]."%' ) ";
+
+                        if($i == ($loopcount-1))
+                        {
+                            $_SESSION["registerClause"].= " ) ";
+                        }else{
+                            $_SESSION["registerClause"].= " OR ";
+                        }
+                    }
+                }
                 $rights = json_decode($output, true);
-                
+
                 $query_module = "SELECT `view` from modules ";
                 $result_module =  mysqli_query($conn, $query_module);
-                while($row_module=mysqli_fetch_array($result_module, MYSQLI_ASSOC))
-                {
-                    $temp=$row_module["view"];
+                while ($row_module = mysqli_fetch_array($result_module, MYSQLI_ASSOC)) {
+                    $temp = $row_module["view"];
                     $_SESSION["rights"][$temp] = $rights[$temp];
                 }
-              
+
                 $_SESSION["sessiontime"] = 6000;
                 $_SESSION["logintime"] = strtotime(date("Y-m-d H:i:s"));
-                echo ('<script>window.location.href ="' .$link."://". $host . '"</script>');
-
-
-
+                echo ('<script>window.location.href ="' . $link . "://" . $host . '"</script>');
             } else {
-               echo ('<script>alert("User Name or Password is wrong"); window.location.href ="' . $_SERVER['HTTP_REFERER'] . '"</script>');
+                echo ('<script>alert("User Name or Password is wrong"); window.location.href ="' . $_SERVER['HTTP_REFERER'] . '"</script>');
             }
         } else {
-                echo ('<script>alert("User Not Found"); window.location.href ="' . $_SERVER['HTTP_REFERER'] . '"</script>');
+            echo ('<script>alert("User Not Found"); window.location.href ="' . $_SERVER['HTTP_REFERER'] . '"</script>');
         }
     } // connection 
 
 
 } // checking url
-else{
+else {
     echo ('<script>alert("UnAuthorized Access"); window.location.href ="' . $_SERVER['HTTP_REFERER'] . '"</script>');
-
 }
--- test.php
+++ test.php
@@ -1,44 +1,9 @@
+<?php
+
+session_start();
 
 
-<!DOCTYPE html>
-<html>
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
-     
-    var myGetUrl = {};
-    var myGetUrl2 = {};
-    myGetUrl["data1"]="1";
-    myGetUrl["data2"]="2";
-    myGetUrl["data3"]="3";
-    myGetUrl["data4"]="4";
-    myGetUrl["data5"]="5";
-    myGetUrl2["data"]=myGetUrl;
-$(document).ready(function(){
-  $("button").click(function(){
-    $.ajax({
-      type: "POST",
-      url: 'http://localhost/testapi.php',
-      data: myGetUrl2,
-      async: false,
-         
-      success: function (data) {
-      $('#div1').html(data);
-      },
-      error: function (xhr) {
-        // document.getElementById("spinnermodal").style.display = "none";
-        //  Server_Response_Fail(xhr.responseText);
-      }
-    });
-  });
-});
-</script>
-</head>
-<body>
+echo($_SESSION["registerClause"]);
 
-<div id="div1"></div>
 
-<button>Get External Content</button>
-
-</body>
-</html>
+?>
