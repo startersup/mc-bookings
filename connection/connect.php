628924a681b83f8bd2a369eda1797fdfed709413
BugHunter1205 <144123064+BugHunter1205@users.noreply.github.com>
2023-09-05 18:09:26 +0530

    Update connect.php
    create connection

--- connection/connect.php
+++ connection/connect.php
@@ -1,6 +1,7 @@
 <?php
-//hi
-$conn=mysqli_connect('sql131.main-hosting.eu','u678426119_mini','Minicabee@123','u678426119_mini');
+//srv671.hstgr.io
+//sql131.main-hosting.eu
+$conn=mysqli_connect('srv671.hstgr.io','u678426119_mini','Minicabee@123','u678426119_mini');
 if(!$conn)
 {
     echo("Conn Failed");
