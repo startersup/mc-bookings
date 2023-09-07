10f835630cc05005b457cbc22a2ae9de8dc1c420
Saicharan <saicharan14996@gmail.com>
2023-09-07 11:58:21 +0530

    few more UI changes

--- navbar.php
+++ navbar.php
@@ -6,8 +6,8 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="./"><img class="mc-nav-logo" src="./assets/images/icons/fleet.svg"><span></span>
-      <!-- <div class="mc-buyer-name"></div> -->
+      <a class="xd-navbar-brand" href="./"><img class="mc-nav-logo" src="./assets/images/icons/fleet.svg"><span></span>
+      <div class="mc-buyer-name">Atlas Cars Pvt. Ltd.</div>
       </a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
@@ -16,24 +16,11 @@
         <li><a class="mc-load li_sidebar" href="drivers">Drivers</a></li>
         <li><a class="mc-load li_sidebar" href="partners">Partners</a></li>
         <li><a class="mc-load li_sidebar" href="invoice">Invoice</a></li>
+        <li><input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for Bookings, Names, Passsengers....." title="Type in a name"></li>
       </ul>
       
       <ul class="nav navbar-nav navbar-right">
-        <li><input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for Bookings, Names, Passsengers....." title="Type in a name"></li>
-            <!-- <li class="dropdown"><a href=""  class="dropdown-toggle custom-arrow">Help</a>
-                <div class="mc-dropdown-content mc-bx-shdw">
-                <ul class="mc-drop-desc">
-                    <li>Help Docs</li>
-                    <li>Feedback</li>
-                     <li>Raise Ticket</li>
-                    <li>Live Chat</li>
-                </ul>
-                </div>
-        </li>
-        <li>
-          <div class="mc-notify-badge">12</div>
-          <a href="" data-toggle="modal" data-target="#notification-bar">Notifications</a>
-        </li> -->
+      
           <li class="dropdown">
             <a href="#"><img src="./assets/images/mc-male-avathar.png" alt="Avatar" class="mc-avatar-bar dropdown-toggle"></a>
             <div class="mc-dropdown-content mc-bx-shdw" style="right:10px;">
